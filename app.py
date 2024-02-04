c598b581e2f4c31cf244c548ae21d3ebef241d8a
advenae <128042036+advenae@users.noreply.github.com>
2024-02-04 18:05:26 +0100

    added card reveal

--- app.py
+++ app.py
@@ -34,26 +34,28 @@ class Deck:
 
         # Add special cards
         special_cards = [
-            # (5, 'W'), (5, 'W'), (5, 'W'),
+            (5, 'W'), (5, 'W'), (5, 'W'),
             (6, 'P'), (6, 'P'), (6, 'P'),
         ]
         for number, action in special_cards:
             self.cards.append(Card(number, action))
 
-        # Shuffle the deck
         random.shuffle(self.cards)
 
 
 class Game:
-    def __init__(self, num_players):
-        self.num_players = num_players
-        self.players = [[] for _ in range(num_players)]
+    def __init__(self):
+        self.num_players = 2
+        self.players = [[] for _ in range(self.num_players)]
         self.face_down_pile = []
         self.face_up_pile = []
         self.current_player = 0
+        self.turn_counter = 0
 
     def switch_to_next_player(self):
         self.current_player = (self.current_player + 1) % self.num_players
+        self.turn_counter += 1
+
     def deal_cards(self, deck):
         for _ in range(4):
             for player in self.players:
@@ -66,28 +68,25 @@ class Game:
     def take_face_up_card(self, exchange_index):
         new_card = self.face_up_pile.pop()
 
-        # Player chooses which card to exchange
         if 0 <= exchange_index < len(self.players[self.current_player]):
             old_card = self.players[self.current_player].pop(exchange_index)
             self.players[self.current_player].insert(exchange_index, new_card)
 
-            # Place the old card on top of the face-up pile
-            self.face_up_pile.insert(0, old_card)
+            self.face_up_pile.append(old_card)
 
             self.switch_to_next_player()
 
             return old_card
         else:
-            # Invalid exchange_index
             return None
 
     def fill_piles(self):
-        while len(self.face_down_pile) < 1 and deck.cards:
+        while len(self.face_down_pile) < 2 and deck.cards:
             self.face_down_pile.append(deck.cards.pop(0))
 
     def take_face_down_card(self, exchange_index):
         if not self.face_down_pile or not self.players[self.current_player]:
-            return None  # No cards in the face-down pile or player's hand
+            return None
 
         new_card = self.face_down_pile.pop()
 
@@ -99,22 +98,19 @@ class Game:
             self.fill_piles()
             self.switch_to_next_player()
 
-            # Check if the face-down pile is empty after the exchange
             if not self.face_down_pile:
                 self.end_game()  # End the game if the face-down deck is empty
 
             return old_card
         else:
-            # Invalid exchange_index
             return None
 
     def leave_face_down_card(self):
         if not self.face_down_pile:
-            return None  # No cards in the face-down pile
+            return None
 
         card = self.face_down_pile.pop()
 
-        # Perform actions based on the game rules
         self.face_up_pile.append(card)
 
         self.fill_piles()
@@ -123,39 +119,48 @@ class Game:
         return card
 
     def end_game(self):
-        # Calculate the sum of points for each player
-        player_sums = [sum(card.number for card in player_hand) for player_hand in self.players]
-
-        # Find the winner (player with the lowest sum of points)
-        winner_index = min(range(len(player_sums)), key=player_sums.__getitem__)
-
-        # Redirect to the end screen
-        return redirect(url_for('end_screen', players=self.players, sums=player_sums, winner=winner_index))
-
+        return redirect(url_for('end_screen'))
 
 @app.route('/new_game', methods=['POST'])
 def new_game():
-    global game, deck  # Assuming you want to reset the game and deck globally
-    game = Game(num_players=2)
+    global game, deck
+    game = Game()
     deck = Deck()
     deck.generate_deck()
     game.deal_cards(deck)
     game.reveal_deck_cards(deck)
 
     return redirect(url_for('game'))
+
 @app.route('/end_screen', methods=['GET', 'POST'])
 def end_screen():
     # Calculate the sum of points for each player
     player_sums = [sum(card.number for card in player_hand) for player_hand in game.players]
 
+    # Determine the highest count of the number 9
+    max_nine_count = max(sum(1 for card in player_hand if card.number == 9) for player_hand in game.players)
+
     # Find the winner (player with the lowest sum of points)
     winner_index = min(range(len(player_sums)), key=player_sums.__getitem__)
 
+    # Calculate player_nine_counts within the endpoint
+    player_nine_counts = [sum(1 for card in player_hand if card.number == 9) for player_hand in game.players]
+
+    # Check if there is a clear winner with the highest count of the number 9
+    if max_nine_count > 0 and sum(1 for count in player_nine_counts if count == max_nine_count) == 1:
+        # Adjust the score only for the player with the highest count of the number 9
+        for i in range(len(game.players)):
+            if player_nine_counts[i] == max_nine_count:
+                nine_sum = 9 * max_nine_count
+                player_sums[i] -= nine_sum
+
     return render_template('end_screen.html', players=game.players, sums=player_sums, winner=winner_index)
+
+
 
 @app.route('/game', methods=['GET', 'POST'])
 def game():
-    revealed = False  # Initialize revealed status
+    revealed = False
     if request.method == 'POST':
         action = request.form['action']
 
@@ -165,12 +170,11 @@ def game():
         elif action == 'take_face_down':
             exchange_index = int(request.form['exchange_index'])
             card = game.take_face_down_card(exchange_index)
-            revealed = True  # Set revealed status to True
+            revealed = True
         elif action == 'leave_face_down':
             card = game.leave_face_down_card()
-            revealed = False  # Reset revealed status
+            revealed = False
 
-        # Check if the game has ended, and if so, redirect to the end screen
         if game.face_down_pile == []:
             return game.end_game()
 
@@ -179,17 +183,19 @@ def game():
                                current_player=game.current_player,
                                face_down_top=game.face_down_pile[-1] if game.face_down_pile else None,
                                face_up_top=game.face_up_pile[-1] if game.face_up_pile else None,
-                               revealed=revealed)  # Pass revealed status to the template
+                               revealed=revealed,
+                               turn_counter=game.turn_counter)
 
     return render_template('game.html',
-                           players=game.players,
-                           current_player=game.current_player,
-                           face_down_top=game.face_down_pile[-1] if game.face_down_pile else None,
-                           face_up_top=game.face_up_pile[-1] if game.face_up_pile else None,
-                           revealed=revealed)  # Pass revealed status to the template
+                               players=game.players,
+                               current_player=game.current_player,
+                               face_down_top=game.face_down_pile[-1] if game.face_down_pile else None,
+                               face_up_top=game.face_up_pile[-1] if game.face_up_pile else None,
+                               revealed=revealed,
+                               turn_counter=game.turn_counter)
 
 if __name__ == '__main__':
-    game = Game(num_players=2)
+    game = Game()
 
     deck = Deck()
     deck.generate_deck()
